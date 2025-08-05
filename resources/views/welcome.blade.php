366409a094e974186cef95434f92bb7a7d01adab
BrnDedEnginr <167724484+BrnDedEnginr@users.noreply.github.com>
2025-08-05 11:48:45 +0200

    Added simple navbar with side-menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-layout>
+    <h1>Portfolio in the works</h1>
+</x-layout>
 
-        <title>Elvin Bajric</title>
-    </head>
-    <body>
-        <h1>Portfolio in the works</h1>
-    </body>
-</html>
